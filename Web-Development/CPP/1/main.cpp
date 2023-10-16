82185bfc8a9a3e96b1a1e2205c00a5166f1ff3c9
rama-27 <ramarajumantena231@gmail.com>
2023-10-16 18:16:12 +0530

    so this is my comment

---
+++ Web-Development/CPP/1/main.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b,c,g;
+    cout<<"enter the numbers";
+    cin>>a>>b>>c;
+    g=a>b?a>c?a:c:b>c?b:c;
+    cout<<"the biggest the number is "<<g;
+
+
+    }
+
---
+++ Web-Development/Java_Script/main.js
@@ -0,0 +1,14 @@
+
+  var myName = "raju"
+
+var myStrg = "My name is " + myName + " and I'm asian"
+
+console.log(myStrg)
+
+
+var a = 3;
+var b = 4;
+console.log(a+b)
+
+a += 5;
+console.log(a)
